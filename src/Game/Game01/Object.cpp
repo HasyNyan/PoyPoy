a4e2e4538f6f4a45a9539d4c8488a50c98785e32
504-23\cre <a>
2025-06-21 19:43:25 +0900

    オブジェクトの表示

--- src/Game/Game01/Object.cpp
+++ src/Game/Game01/Object.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include "Object.h";
+#include <System/Component/ComponentModel.h>
+namespace Game01 {
+bool Stone::Init()
+{
+    __super::Init();
+
+    SetName("ストーン");
+    SetTranslate({0.0f, 10.0f, 0.0f});
+
+    return true;
+}
+void Stone::Draw()
+{
+    Super::Draw();
+    //描画する位置を取得
+    auto pos = cast(GetTranslate());
+    //円の色
+    auto color = GetColor(0, 255, 255);
+    //描画
+    DrawSphere3D(pos, 3.0f, 20, color, color, TRUE);
+}
+}    // namespace Game01
