1cb1f0357572937046e4c7bac3e5c9f06ae54dea
504-22\cre <a>
2025-06-21 19:41:41 +0900

    playerUI途中

---
+++ src/Game/Game01/PlayerUI.cpp
@@ -0,0 +1,22 @@
+﻿#include "Player.h"
+#include "PlayerUI.h"
+namespace Game01 {
+bool PlayerUI::Init()
+{
+    return true;
+}
+void PlayerUI::Update()
+{
+}
+void PlayerUI::Draw()
+{
+    // とりあえずTitleという文字を表示しておく
+    DrawFormatString(100, 50, GetColor(255, 255, 255), "テスト");
+}
+void PlayerUI::Exit()
+{
+}
+void PlayerUI::GUI()
+{
+}
+}    // namespace Game01
---
+++ src/Game/Game01/PlayerUI.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <System/Scene.h>
+namespace Game01 {
+//! @brief チュートリアルシーン
+class PlayerUI : public Scene::Base
+{
+public:
+    BP_CLASS_DECL(PlayerUI, u8"Game01 の PlayerUI");
+
+    //! @brief 初期化
+    //! @return 初期化済み
+    bool Init() override;
+    void Update() override;
+    void Draw() override;
+    void Exit() override;
+    void GUI() override;
+
+private:
+    // 必要であれば変数をここに追加する
+};
+}    // namespace Game01
+
+// namespace Game01
