116d681eafe59dc03e4e970b99045fde09b9d595
YOSHII3027\touma <oca2402sg0025@edu.oca.ac.jp>
2025-07-04 01:24:31 +0900

    HPSet

--- src/Game/Game01/PlayerUI.cpp
+++ src/Game/Game01/PlayerUI.cpp
@@ -4,10 +4,14 @@ namespace Game01 {
 bool PlayerUI::Init()
 {
     __super::Init();
+    m_hpx  = 170.0f;
+    m_hpy  = 610.0f;
+    m_hpx2 = 270.0f;
     return true;
 }
 void PlayerUI::Update()
 {
+    SetHp();
 }
 void PlayerUI::Draw()
 {
@@ -19,6 +23,10 @@ void PlayerUI::Draw()
         SetFontSize(40);
         DrawFormatString(50 + 300 * i, 590, GetColor(255, 255, 0), "%dP", i + 1);
 
+        //HPの描画
+        DrawTriangleAA(m_hpx + 300.0f * i, m_hpy, m_hpx2 + 300.0f * i, m_hpy, 220.0f + 300.0f * i, 670.0f, GetColor(255, 0, 0), TRUE);
+
+        //HPの外枠の描画
         DrawTriangleAA(170.0f + 300.0f * i, 610.0f, 270.0f + 300.0f * i, 610.0f, 220.0f + 300.0f * i, 670.0f, GetColor(0, 0, 0), FALSE);
     }
 }
@@ -27,6 +35,35 @@ void PlayerUI::Exit()
     Super::Exit();
 }
 void PlayerUI::GUI()
+{
+    Super::GUI();
+}
+void PlayerUI::SetHp()
+{
+    if(CheckHitKey(KEY_INPUT_UP)) {
+        m_hpx -= 0.8f;
+        m_hpy--;
+        m_hpx2 += 0.8f;
+    }
+    if(CheckHitKey(KEY_INPUT_DOWN)) {
+        m_hpx += 0.8f;
+        m_hpy++;
+        m_hpx2 -= 0.8f;
+    }
+    if(m_hpx > 220.0f)
+        m_hpx = 220.0f;
+    if(m_hpx < 170.0f)
+        m_hpx = 170.0f;
+    if(m_hpx2 < 220.0f)
+        m_hpx2 = 220.0f;
+    if(m_hpx2 > 270.0f)
+        m_hpx2 = 270.0f;
+    if(m_hpy > 670.0f)
+        m_hpy = 670.0f;
+    if(m_hpy < 610.0f)
+        m_hpy = 610.0f;
+}
+void PlayerUI::SetMp()
 {
 }
 }    // namespace Game01
