2f7e21dec75f124b817db7457b01fd488324b503
DESKTOP-LJ51TPT\PC_User <oca2402sg0014@edu.oca.ac.jp>
2025-07-06 20:03:19 +0900

    オブジェクト生成器の表示

--- src/Game/Game01/Ufo.cpp
+++ src/Game/Game01/Ufo.cpp
@@ -19,11 +19,9 @@ void Ufo::Draw()
     auto pos = cast(GetTranslate());
     //Ufoの色
     auto color = GetColor(255, 255, 255);
-    //描画[
-
-    for(int i = 1; i < 4; i++) {
-        DrawTriangle3D(VGet(5.0f, 10.0f + 5.0 * i, 5.0f), VGet(5.0f, 10.0f + 5.0 * i, 0.0f), VGet(0.0f, 10.0f + 5.0 * i, 5.0f), GetColor(255, 255, 255), TRUE);
-    }
+    //描画
+    DrawTriangle3D(VGet(5.0f, 15.0f, 5.0f), VGet(5.0f, 15.0f, 0.0f), VGet(0.0f, 15.0f, 5.0f), GetColor(255, 255, 255), TRUE);    //右側の三角形
+    DrawTriangle3D(VGet(0.0f, 15.0f, 0.0f), VGet(5.0f, 15.0f, 0.0f), VGet(0.0f, 15.0f, 5.0f), GetColor(255, 255, 255), TRUE);    //左側の三角形
 }
 
 }    // namespace Game01
